f69733d815186f2a32c597a3f4122cd013fd8b6f
MoralesKevin <moraleskevin1502@gmail.com>
2018-10-12 08:58:58 +0200

    ajout de la page Score

---
+++ src/Felicitation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BonnesReponses from './BonnesReponses';
+
+class Felicitation extends Component {
+    render() {
+        return ( 
+            <div className="Felicitation">
+                <p>BRAVO !</p>
+                <BonnesReponses />
+            </div>
+         );
+    }
+}
+
+export default Felicitation;
